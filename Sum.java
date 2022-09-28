87f91dd90c2a9330cabf25947f3092a38a85a5a3
Tanisha Kriplani <kriplanitanisha2002@gmail.com>
2022-09-28 20:57:46 +0530

    Add in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,7 @@ public class Sum{
     public static void main(String[] args){
         int a=3;
         int b= 4;
+        System.out.println("sum");
         System.out.println("The sum of two digit" + (a+b));
     }
 }
