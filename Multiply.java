fe83ca6e559b3d77206ad0ffb0fd6da2c238d78a
Tanisha Kriplani <kriplanitanisha2002@gmail.com>
2022-09-28 21:16:45 +0530

    Add Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+
+    public static void main(String[] args){
+        int a=3;
+        int b= 4;
+        System.out.println("The product of two digit" + (a*b));
+    } 
+}
